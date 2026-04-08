4012099a326d44f4c7b1fdce839bd8d949beea59
Degtyarev02 <vladimirdegty@gmail.com>
2026-04-08 07:58:02 +0300

    main

--- src/Gifts.js
+++ src/Gifts.js
@@ -12,13 +12,21 @@ const Gifts = () => {
 			sx={{ height: "auto", pb: 6, px: matches ? 4 : 2 }}
 			size={{ xs: 12 }}
 		>
-			<Grid item size={{ xs: 12}}>
+			<Grid item size={{ xs: 12 }}>
 				<InfoContainer
 					elevation={0}
 					sx={{ background: 'url("/background.png")' }}
 				>
 					<Box sx={{ display: "flex", flexDirection: "column", mb: 3 }}>
-						<InfoContainer sx={{ height: "auto", mb: 3, display: "flex", alignItems: "center", padding: "20px" }}>
+						<InfoContainer
+							sx={{
+								height: "auto",
+								mb: 3,
+								display: "flex",
+								alignItems: "center",
+								padding: "20px",
+							}}
+						>
 							<NotInterested
 								sx={{
 									fontSize: "2rem",
@@ -39,11 +47,12 @@ const Gifts = () => {
 								variant="h5"
 								sx={{ color: theme.palette.text.primary, padding: "20px" }}
 							>
-								Нам будет негде разместить цветы после росписи. <br /> Вместо букета
-								цветов вы можете принести бутылочку вина с пожеланием на какой день нам
-								ее открыть. <br /> Если вы не хотите дарить алкоголь, можете
-								заказать в ближайший к мкр. Солнцево-парк ПВЗ пакет корма для
-								собак, а мы отвезем его в приют.
+								Нам будет негде разместить цветы после росписи, поэтому они
+								могут быстро завять.
+								<br /> Вместо букета цветов вы можете принести бутылочку вина с
+								пожеланием, на какой день нам ее открыть. <br /> Или заказать в
+								ближайший к метро Пыхтино ПВЗ пакет корма для собак или кошечек,
+								а мы отвезем его в приют.
 							</Typography>
 						</InfoContainer>
 					</Box>
